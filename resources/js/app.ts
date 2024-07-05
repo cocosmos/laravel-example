31443dece99a553eb39ce9cecbd12cde9e93adec
cocosmos <mipam.guillot@crea-inseec.com>
2024-07-05 23:11:20 +0200

    chore(update): laravel v11 + image docker

---
+++ resources/js/app.ts
@@ -0,0 +1,26 @@
+import './bootstrap';
+import '../../../laravel-example/resources/css/app.css';
+
+import { createApp, h, DefineComponent } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from 'ziggy-js';
+import i18n from "@/i18n";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(i18n)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#d99767',
+    },
+});
+
