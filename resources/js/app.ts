c7b64b9a55fe6f773cca6d723796fe220e925928
cocosmos <mipam.guillot@crea-inseec.com>
2024-11-24 21:06:41 +0100

    chore(11): update to laravel 11

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,17 +1,23 @@
+import '../css/app.css';
 import './bootstrap';
-import '../../../laravel-example/resources/css/app.css';
 
-import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from 'ziggy-js';
+import { createApp, DefineComponent, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import i18n from "@/i18n";
+import 'material-icons/iconfont/filled.css';
+import 'material-icons/iconfont/outlined.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
@@ -20,7 +26,6 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: '#d99767',
+        color: '#4B5563',
     },
 });
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -15,35 +16,8 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            boxShadow: {
-                '3xl': 'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px',
-            }
         },
     },
 
-    plugins: [
-        require("@tailwindcss/typography"),
-        require("daisyui"),
-    ],
-
-    daisyui: {
-        themes: [
-            "bumblebee",
-            {
-                "jaigopal": {
-                    "primary": "#EFEDE4", //#fcd34d
-                    "secondary": "#e3dfd3",
-                    "accent": "#debc74", //#543D37
-                    "neutral": "#4a4657",
-                    "base-100": "#f9f9f9", //#F6F1EE
-                    "info": "#85a8d6",
-                    "success": "#22c564",
-                    "warning": "#f6cb2c",
-                    "error": "#e6513d",
-                }
-            }
-
-        ],
-
-    }
+    plugins: [forms, typography],
 };
